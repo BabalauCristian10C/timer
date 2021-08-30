e7618f4a498c49475a3924dcae1889663cdb8cf0
Chris Babalau <chrisbabalau@Chriss-MacBook-Air.local>
2021-08-30 14:50:16 +0300

    second committ

--- script.js
+++ script.js
@@ -313,4 +313,89 @@ window.addEventListener("DOMContentLoaded", function() {
     };
 
     slider();
+
+    const ourteam = () =>{
+        const container = document.querySelector(".command"),
+            teamMember = container.querySelectorAll('.command__photo');
+        let arr = new Array();
+        teamMember.forEach(member =>{
+            member.addEventListener('mouseenter', (e)=>{
+                const target = e.target,
+                    img = target.src,
+                    dimg= target.dataset.img;
+                    arr = [img, dimg];
+                target.src = arr[1];
+                target.dataset.img = arr[0];  
+            })
+            member.addEventListener('mouseleave', (e)=>{
+                const target = e.target,
+                    img = target.src,
+                    dimg= target.dataset.img;
+                    arr = [img, dimg];
+                target.src = arr[1];
+                target.dataset.img = arr[0];
+            })
+        })
+    };
+    ourteam()
+
+    const calculator = ()=>{
+        const inputs = document.querySelectorAll(".calc-item");
+        inputs.forEach(item =>{
+            item.addEventListener("input", (e) =>{
+                const target = e.target;
+                target.value = target.value.replace(/[^0-9]/, "")
+            })
+        })
+    };
+    calculator();
+
+    const contact = () =>{
+        const name = document.querySelector("#form2-name"),
+            message = document.querySelector("#form2-message"),
+            email = document.querySelector("#form2-email"),
+            number = document.querySelector("#form2-phone");
+
+        const checkCyr = (event) =>{
+            const target = event.target;
+            target.value = target.value.replace(/[^А-Яа-яЁё\s]/gi, "")
+            target.value = target.value.replace(/\s{2,10000}/g, " ")
+            target.value = target.value.trim();
+        }
+        const checkCyrName = (event) =>{
+            const target = event.target;
+            target.value = target.value.replace(/[^А-Яа-яЁё\s]/gi, "")
+            target.value = target.value.replace(/\s{2,10000}/g, " ")
+            target.value = target.value.trim();
+            target.value = target.value.replace(/([а-я])?[А-Яа-яЁё]*/g, (match,v) => {
+                if(v){
+                    console.log(typeof match)
+                    return v.toUpperCase() + match.substr(1);
+                } else {
+                    return match;
+                }
+                
+            })
+        }
+        const checkEmail = (event) =>{
+            const target = event.target;
+            target.value = target.value.replace(/[^A-z!@._-~*']/gi, "")
+            target.value = target.value.replace(/\s/g, "")
+            target.value = target.value.trim();
+            target.value = target.value.replace(/-{2,10000}/g, "-")
+
+        }
+        const checkNumber = (event) =>{
+            const target = event.target;
+            target.value = target.value.replace(/[^0-9()-]/gi,"")
+            target.value = target.value.replace(/(\-){2,10000}/g, "-")
+            target.value = target.value.trim();
+        }
+
+        name.addEventListener('blur', checkCyrName);
+        message.addEventListener('blur', checkCyr);
+        email.addEventListener("blur",checkEmail);
+        number.addEventListener("blur", checkNumber);
+    }
+    contact();
 })
